24d30d15717975acb818340ec4c6eb3a3820260b
eeemihaeee <69985028+eeemihaeee@users.noreply.github.com>
2021-05-18 21:15:50 +0500

    Добавление проекта

--- ajax.js
+++ ajax.js
@@ -1,13 +1,13 @@
-// ТАБЛИЦА 
+// ТАБЛИЦА на работу 
 $(document).ready(function() {
   $("#bta").click(function() {
-    // задаем функцию при нажатиии на элемент <button>
+    // задаем функцию при нажатиии на элемент <button>.
     $.ajax({
-      method: "POST", // метод HTTP, используемый для запроса
-      url: "screen.php", // строка, содержащая URL адрес, на который отправляется запрос
+      method: "POST", // метод HTTP, используемый для запроса.
+      url: "screen.php", // строка, содержащая URL адрес, на который отправляется запрос.
       data: $("#form :input").serialize(),
       success: function(msg) {
-        // получен ответ сервера
+        // получен ответ сервера.
         $("#vivod").html(msg);
       }
     });
@@ -18,13 +18,13 @@ $(document).ready(function() {
 // ВЫПАДАЮЩЕЕ МЕНЮ
 $(document).ready(function() {
   $("#btn").click(function() {
-    // задаем функцию при нажатиии на элемент <button>
+    // задаем функцию при нажатиии на элемент <button>.
     $.ajax({
-      method: "POST", // метод HTTP, используемый для запроса
-      url: "spisok_table.php", // строка, содержащая URL адрес, на который отправляется запрос
+      method: "POST", // метод HTTP, используемый для запроса.
+      url: "spisok_table.php", // строка, содержащая URL адрес, на который отправляется запрос.
       data: $("#plan :input").serialize(),
       success: function(msg) {
-        // получен ответ сервера
+        // получен ответ сервера.
         $("#block").html(msg);
       }
     });
